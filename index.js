8571eed3467c063c6e8cb394291a0621e1a67a69
Ian Ditsch <IanDitsch@Outlook.com>
2019-07-30 22:31:17 -0500

    Angular Lesson 1

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+// const somethingFun: string = 'Riding my bike is fun!';
+// console.log(somethingFun)
+// function orderFood(sandwich: string, orderNumber: number): void {
+//     console.log('Your order number is ' + orderNumber + ' and contains a ' + sandwich + ' sandwich');
+// }
+// orderFood('Ham & Cheese', 32);
+var tasty = 'that was delicious';
